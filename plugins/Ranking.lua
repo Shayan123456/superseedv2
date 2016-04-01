51dded194e006d9cf3bbd1b88d1535ae2211e11f
Shayan123456 <shayan_jon123@yahoo.com>
2016-04-01 17:30:09 +0430

    Update Ranking.lua

--- plugins/Ranking.lua
+++ plugins/Ranking.lua
@@ -3,7 +3,7 @@
 local apikey = 
 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjYzOTRlNGMwLTY0ZDItNDVlNS1hYmVhLTFkZDY2M2NhMTgwMyIsImlhdCI6MTQ1OTI0MDIzMSwic3ViIjoiZGV2ZWxvcGVyL2EyNTY0N2E4LWQwNjEtZDdlNi1hYjRmLWEzNGE0YTFiMzNkNiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjgyLjEwMi4xMy45OCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.TTNEetGPs4X1_cEWRr1ARBjgz5sXYGjxgI0b4wirgYb2vr6S1m8fQBP8imt9rMGq7D8eafEDZVMn3QiBiSshAg' 
 local function run(msg, matches)
- if matches[1]:lower() == 'clan' or matches[1]:lower() == 'clash' or matches[1]:lower() == 'clantag' or matches[1]:lower() == 'tag' then
+ if matches[1]:lower() == 'clanrank' or matches[1]:lower() == 'clash' or matches[1]:lower() == 'clantag' or matches[1]:lower() == 'tag' then
   local clantag = matches[2]
   if string.match(matches[2], '^#.+$') then
      clantag = string.gsub(matches[2], '#', '')
@@ -39,13 +39,10 @@ if jdat.reason then
  
  
  
- if matches[1]:lower() == 'members' or matches[1]:lower() == 'clashmembers' or matches[1]:lower() == 'clanmembers' then
+ if matches[1]:lower() == 'memberrank' or matches[1]:lower() == 'clashmembers' or matches[1]:lower() == 'clanmembers' then
   local members = matches[2]
-  if string.match(matches[2], '^#.+$') then
-     members = string.gsub(matches[2], '#', '')
-  end
   members = string.upper(members)
-  local curl = 'curl -X GET --header "Accept: application/json" --header "authorization: Bearer '..apikey..'" "https://api.clashofclans.com/v1/clans/%23'..members..'/members"'
+  local curl = 'curl -X GET --header "Accept: application/json" --header "authorization: Bearer <API token>" "https://api.clashofclans.com/v1/locations/32000006/rankings/clans"'
   cmd = io.popen(curl)
   local result = cmd:read('*all')
   local jdat = json:decode(result)
@@ -54,18 +51,30 @@ if jdat.reason then
    return '#Error\n'..jdat.reason
   end
   local leader = ""
-  local coleader = ""
+  local R2 = ""
+  local R3 = ""
+  local R4 = ""
+  local R5 = ""
   local items = jdat.items
-  leader = 'Clan Moderators: \n'
+  leader = 'Members Ranking: \n'
    for i = 1, #items do
-   if items[i].role == "leader" then
-   leader = leader.."\nLeader: "..items[i].name.."\nLevel: "..items[i].expLevel.."\nleague: "..items[i].league.name
+   if items[i].rank == "1" then
+   leader = leader.."\n1: "..items[i].name.."\nLevel: "..items[i].expLevel.."\nleague: "..items[i].league.name
    end
-   if items[i].role == "coLeader" then
-   coleader = coleader.."\nCo-Leader: "..items[i].name.."\nLevel: "..items[i].expLevel.."\nDonations: "..items[i].donations.."\nDonations Recieved: "..items[i].donationsReceived
+   if items[i].rank == "2" then
+   R2 = R2.."\n2: "..items[i].name.."\nLevel: "..items[i].expLevel.."\nleague: "..items[i].league.name
+  end
+   if items[i].rank == "3" then
+   R3 = R3.."\n3: "..items[i].name.."\nLevel: "..items[i].expLevel.."\nleague: "..items[i].league.name
+  end
+     if items[i].rank == "4" then
+   R4 = R4.."\n2: "..items[i].name.."\nLevel: "..items[i].expLevel.."\nleague: "..items[i].league.name
+  end
+     if items[i].rank == "5" then
+   R5 = R5.."\n2: "..items[i].name.."\nLevel: "..items[i].expLevel.."\nleague: "..items[i].league.name
    end
   end
-text = leader.."\n"..coleader.."\n\nClan Members:"
+text = leader.."\n"..R2.."\n"..R3.."\n"..R4"\n"..R5
   for i = 1, #items do
   text = text..'\n'..i..'- '..items[i].name..'\nlevel: '..items[i].expLevel.."\n"
   end
